df7cb3ba82a61bede3d825ae9afa539a311ce093
mishuk09 <mishukinfo09@gmail.com>
2024-02-28 00:14:52 +0530

    add responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 // tailwind.config.js
 module.exports = {
     theme: {
+        extend: {
+        screens: {
+            'xs': '350px',
+            'sm': '640px',
+        },
         container: {
             center: true,
             padding: '2rem',
         },
+    }
     },
-}
+};
