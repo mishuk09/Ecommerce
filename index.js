83b8c0db4f4155d2833ebad87966cebb72fe9c99
mishuk09 <mishukinfo09@gmail.com>
2024-02-28 18:42:34 +0530

    dropdown recode

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 // ####                                            ####
 // ################################################## 
 
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var dropdownButtons = document.querySelectorAll('.dropdown-button');
     var dropdownContents = document.querySelectorAll('.dropdown-content');
@@ -42,8 +44,50 @@ document.addEventListener('DOMContentLoaded', function () {
         dropdownContents[index].addEventListener('mouseleave', function () {
             hideDropdown(this);
         });
+
+        // Add smooth transition effect using CSS
+        dropdownContents[index].style.transition = 'opacity 1s ease, transform 1s ease'; // Change timing and property as needed
     });
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var dropdownButtons = document.querySelectorAll('.dropdown-button');
+//     var dropdownContents = document.querySelectorAll('.dropdown-content');
+
+//     function showDropdown(content) {
+//         content.classList.remove('hidden');
+//     }
+
+//     function hideDropdown(content) {
+//         content.classList.add('hidden');
+//     }
+
+//     dropdownButtons.forEach(function (button, index) {
+//         button.addEventListener('click', function (event) {
+//             event.preventDefault();
+//             var content = dropdownContents[index];
+//             content.classList.toggle('hidden');
+//         });
+
+//         button.addEventListener('mouseenter', function () {
+//             var content = dropdownContents[index];
+//             showDropdown(content);
+//         });
+
+//         button.addEventListener('mouseleave', function () {
+//             var content = dropdownContents[index];
+//             hideDropdown(content);
+//         });
+
+//         dropdownContents[index].addEventListener('mouseenter', function () {
+//             showDropdown(this);
+//         });
+
+//         dropdownContents[index].addEventListener('mouseleave', function () {
+//             hideDropdown(this);
+//         });
+//     });
+// });
 
 
 
