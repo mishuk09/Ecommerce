eaa4fd13dc78902f01e557c9550d0f9b487cbb66
mishuk09 <mishukinfo09@gmail.com>
2024-02-28 18:26:21 +0530

    add footer section

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
+
+// #################################################
+// ####                                            ####
+// ####          Dropdown functionality            ####
+// ####                                            ####
+// ################################################## 
+
 document.addEventListener('DOMContentLoaded', function () {
     var dropdownButtons = document.querySelectorAll('.dropdown-button');
     var dropdownContents = document.querySelectorAll('.dropdown-content');
@@ -39,3 +46,61 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+
+function toggleSearch() {
+    var logossDiv = document.getElementById("logoss");
+    if (logossDiv.style.display === "none") {
+        logossDiv.style.display = "block";
+    } else {
+        logossDiv.style.display = "none";
+    }
+};
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navMenu = document.querySelector('.dropdown-div');
+    const navMenuHeight = navMenu.clientHeight;
+    const scrollThreshold = navMenu.offsetTop; // or set your own threshold
+
+    function handleScroll() {
+        if (window.pageYOffset > scrollThreshold) {
+            navMenu.classList.add('fixed', 'top-0');
+        } else {
+            navMenu.classList.remove('fixed', 'top-0');
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+});
+
+
+
+
+// #################################################
+// ####                                            ####
+// ####          GO TO TOP js              ####
+// ####                                            ####
+// ################################################## 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const goToTopButton = document.getElementById('goToTopButton');
+
+    // Show or hide the button based on scroll position
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 200) {
+            goToTopButton.classList.add('show');
+        } else {
+            goToTopButton.classList.remove('show');
+        }
+    });
+
+    // Smooth scroll to top
+    goToTopButton.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
