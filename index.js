773c53864eaef90be3b8ebc369e974d4b585b1f7
mishuk09 <mishukinfo09@gmail.com>
2024-03-04 23:10:10 +0530

    add hover

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+
 // #################################################
 // ####                                            ####
 // ####          Dropdown functionality            ####
