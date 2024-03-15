0c8e69439b295cf9089ec8b3bc623ebbf3fcf8e6
mishuk09 <mishukinfo09@gmail.com>
2024-03-15 22:54:36 +0530

    add tab content

--- js/products.js
+++ js/products.js
@@ -7,5 +7,18 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $('.tab-content:first').show(); // Show the first tab content by default
+    $('.tab-links a').on('click', function (e) {
+        var currentAttrValue = $(this).attr('href');
+        // Show/Hide Tabs
+        $('.tab-content').hide();
+        $(currentAttrValue).fadeIn();
 
+        // Change/remove current tab to active
+        $('.tab-links li').removeClass('active').addClass('inactive');
+        $(this).parent('li').removeClass('inactive').addClass('active');
 
+        e.preventDefault();
+    });
+});
